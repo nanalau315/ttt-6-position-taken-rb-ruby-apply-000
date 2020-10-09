6b4528a32f6543e547bf75c919f2eea0c99d15bc
Chamaeleonidae Na <nanalau315@hotmail.com>
2020-10-09 02:51:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,11 +3,22 @@
 
 
 def position_taken? (board, index)
+<<<<<<< HEAD
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
    
   else
     true
     
+=======
+  if board[index] == " " || "" || nil
+    puts false
+    false
+    
+   
+  else board[index] == "X" || "O"
+    puts true
+    true    
+>>>>>>> 602f7dae1db5caf447ecf83fec9b40cb5c849484
   end
 end
